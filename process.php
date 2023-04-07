e4234625774a89aa8cfeb235ae8bad8ea0f2c024
arashdeepsinghdhami <122625724+arashdeepsinghdhami@users.noreply.github.com>
2023-04-07 18:50:29 -0400

    Add files via upload

---
+++ process.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <html lang="en">
+  <head>
+   <meta utf-8/>
+    <title>
+      
+        order conformation
+      
+    </title>
+    <link rel="stylesheet" href="process.css"/>
+  </head>
+  <body>
+    <fieldset>
+<header>
+      <h1>
+      <p>your order for mobile is confirmed </p>
+      <p>if you want any other things and information we also share our contact information</p>
+        <p>64789837273</p>
+        <p>00038979797</p>
+        <p>98376474474</p>
+        
+      </h1>
+  <?php
+$name="Dhami mobile shop";
+ 
+  
+echo("Thanks for choosing.please pay".$price."at checkout");
+?>
+
+    </header>
+      </fieldset>
+  </body>
+</html>
